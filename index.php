5bf30cfdad2fe3586d046c46536ae1708c6a4319
Ranjtih Siji <ranjith.sajeev@gmail.com>
2017-04-03 14:03:33 +0530

    Adding Cover Page and Edit a thons

--- index.php
+++ index.php
@@ -8,19 +8,8 @@
     <title>Woman's Edit A Thon Statistics - Malayalam Wikipedia - Wikipedia Edit a thon Staticstics App</title>
     <meta name="Description"  content="Wikipedia Edit A Thon statistics app using angular js and quarry">
     <link rel="icon" type="image/x-icon" href="favicon.png">
-    <script src="js/angular.js"></script>
-    <script src="js/angular-touch.js"></script>
-    <script src="js/angular-animate.js"></script>
-    <script src="js/csv.js"></script>
-   <script src="js/pdfmake.js"></script>
-    <script src="js/vfs_fonts.js"></script>
-    <script src="js/ui-grid.js"></script>
-   <link rel="stylesheet" href="css/ui-grid.css" type="text/css">
-   <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
-   <script src="js/Chart.min.js"></script>
-   <script src="js/angular-chart.js"></script>
-    <script src="appgrid.js"></script>
-    <style>
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+     <style>
     body {padding-top: 50px;}
     .page {
       width: 1200px;
@@ -37,7 +26,7 @@
       }
 			a {text-decoration:none;} h1{padding-top:50px;}
       #mainBox {height: 250px;}
-      #mainHd {background: url('img/fountain-pens.jpg') right bottom fixed; background-size: cover; }
+      #mainHd {background: url('img/pens.jpg') right bottom fixed; background-size: cover; }
       #mainHd h1 {color: #fff; font-weight: bold; text-shadow: 0px 0px 4px #333;}
       #mainHd p {color: #fff;
             font-size: 1.4em;
@@ -53,6 +42,12 @@
           margin-top: 70px;
           border-top: 1px solid #0048de;
           background-color: #dedede;
+      }
+      .onHead {
+      	font-weight: bold; font-size: 3.4em; color: #2780E3;padding: 10px 5px; border-bottom: 1px solid #aaa; margin-bottom: 20px;
+      }
+      .itmList {
+      	border: 1px solid #2780E3; margin:10px 2px; padding: 25px; font-size: 1.8em; margin:20px 5px; color: #333;
       }
 	</style>
   </head>
@@ -75,110 +70,41 @@
               Wiki Stats
             </a>
           </li>
-          <li><a href="#getting_started">Data</a></li>
-          <li><a href="#userwise">Users</a></li>
-          <li><a href="#datewise">Date Wise</a></li>
-          <li><a href="#userdata">Wiki</a></li>
-          <li><a href="#credits">Details</a></li>
+          <li><a href="#getting_started">Malayalm</a></li>
+          <li><a href="#userwise">2017</a></li>
+          <li><a href="#datewise">2016</a></li>
+          <li><a href="#userdata">Odia</a></li>
+          <li><a href="#credits">2017</a></li>
         </ul>
       </nav>
     </div>
   </div>
-<div class="container-fluid" id="mainHd">
+  <div class="container-fluid" id="mainHd">
   <div class="text-center aspect-ratio" id="mainBox">
     <div class="header">
         <h1>
-          Statistics of Women's Day Edit a Thon 2017
+          Edit a thons in Wikipedia - Malayalam, Odia
         </h1>
 
-        <p>Staticstics of Women's Day Edit a Thon 2017 in <a href="http://ml.wikipedia.org/wiki/WP:WHMIN17">Malayalam Wikipedia</a> based on <a href="http://www.chartjs.org">Chart.js</a></p>
+        <p>Staticstics of Edit a thons in  <a href="http://ml.wikipedia.org/wiki/">Malayalam Wikipedia</a></p>
 
     </div>
   </div>
 </div>
 <div class="container">
-  <div class="page-header">
-  <div ng-controller="wikiCtrl">
-      <div class="row">
-        <div class="col-lg-12 col-sm-12">
-            <p class="big">Total {{artnumber}} Articles </p>
-            <p class="centerp">From 1st March 2017 to 31st March 2017</p>
+<div class="row">
+        <div class="col-lg-12 col-sm-12 text-center ">
+        	<h2 class="onHead">Malayalam Wikipedia 2017</h2>
+        	<p class="itmList"><a href="books17/index.php">International Books Day Edit a thon 217</a></p>
+        	<p class="itmList"><a href="whmin2017/index.php">International Woman's Day Edit a thon 2017</a> 745 Articles, 34 Users</p>
+        </div>	
+</div>
+<div class="row">
+        <div class="col-lg-12 col-sm-12 text-center ">
+        <h2 class="onHead">Odia Wikipedia 2017</h2>
+        <p class="itmList">International Woman's Day Edit a thon 2017</p>
         </div>
-      </div>
-      <div class="row" id="getting_started">
-        <div class="col-lg-12 col-sm-12">
-          <h2>Articles created in this Edit A Thon</h2>
-          <div ui-grid="gridOptions" class="grid" ui-grid-resize-columns ui-grid-move-columns ui-grid-auto-resize></div>
-        </div>
-      </div>
-      <span id="userwise">.</span>
-      <hr/>
-        <h2>User Staticstics </h2>
-      <div class="row">
-        <div class="col-lg-3 col-md-4 col-sm-12">
-          <table class="table table-striped table-bordered">
-            <tr>
-              <th>No</th>
-              <th>User Name</th>
-              <th>Number <br/>of <br/>Articles</th>
-            </tr>
-            <tr ng-repeat="value in users">
-              <td>{{$index + 1}}</td>
-              <td>{{value.User}}</td>
-              <td>{{value.ArticleNo}}</td>
-            </tr>
-          </table>
-        </div>
-        <div class="col-lg-9 col-md-8 col-sm-12">
-          <div class="graph1">
-          <canvas id="bar" class="chart chart-bar"
-    chart-data="data" chart-labels="labels" chart-series="series"></canvas>
-          </div>
-        </div>
-      </div>
-      <span id="datewise">.</span>
-      <hr/>
-        <h2 >Date Wise Article Staticstics </h2>
-      <div class="row">
-        <div class="col-lg-9 col-md-8 col-sm-12">
-          <div class="graph1">
-            <canvas id="line" class="chart chart-line" chart-data="dtdata" 
- chart-labels="dtlabels" chart-series="dtseries" chart-options="dtoptions"
- chart-dataset-override="dtdatasetOverride" chart-colors="colors" chart-click="onClick"></canvas>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-4 col-sm-12">
-            <table class="table table-striped table-bordered">
-            <tr>
-              <th>No</th>
-              <th>Date</th>
-              <th>Number <br/>of <br/>Articles</th>
-            </tr>
-            <tr ng-repeat="value in datez">
-              <td>{{$index + 1}}</td>
-              <td>{{value.Date}}</td>
-              <td>{{value.ArticleNo}}</td>
-            </tr>
-          </table>
-        </div>
-      </div>
-      <span id="userdata">.</span>
-      <hr/>
-        <h2 >Users contribution percentage </h2>
-      <div class="row">
-        <div class="col-lg-6 col-md-6 col-sm-12">
-          <div style="width:330px;">
-            <canvas id="pie" class="chart chart-pie"
-      chart-data="data" chart-labels="labels" chart-options="options"></canvas>
-          </div>
-        </div>
-        <div class="col-lg-6 col-md-6 col-sm-12">
-          <h3>About Woman's Day Edit A Thon</h3>
-          <p>Woman's Day Edit A Thon is from 1st March 2017 to 31st March 2017<br/>on Malayalam Wikipedia.</p>
-        </div>
-      </div>
-    </div>
-  </div>
+</div>
 </div>
 <span id="credits">.</span>
 <footer class="footer">
